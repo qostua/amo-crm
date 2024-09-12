e0038108d2b785b1bb66a221f9e26a46cdf4613a
qostua <qostua@gmail.com>
2024-09-12 05:17:53 +0300

    connect files to js entry point

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,22 @@
-// const myHeaders = new Headers();
-// myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6ImE2MzY3NDNiMDc4NjNmOTg5ZjM2Y2JiYWYxODUwYzBmMmU0MWYyMzM4ODljOGUxNzRlYjZmM2I3NDUxNWRhMjZmMWFkYmVlY2ZlNzIwMGNjIn0.eyJhdWQiOiI5ZWQ3YTM2YS1lYTNiLTRiMzEtYTMzYi0zYzU2MzE0YThmMjkiLCJqdGkiOiJhNjM2NzQzYjA3ODYzZjk4OWYzNmNiYmFmMTg1MGMwZjJlNDFmMjMzODg5YzhlMTc0ZWI2ZjNiNzQ1MTVkYTI2ZjFhZGJlZWNmZTcyMDBjYyIsImlhdCI6MTcyNTk5MzE0NCwibmJmIjoxNzI1OTkzMTQ0LCJleHAiOjE3MzMyNzA0MDAsInN1YiI6IjExNDk2NzQyIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjMxOTQxNDQ2LCJiYXNlX2RvbWFpbiI6ImFtb2NybS5ydSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJjcm0iLCJmaWxlcyIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiLCJwdXNoX25vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiMmU3MzFhNmMtMmFhZi00ZDc3LTllZTAtZDllYTIwMDJmOTU2IiwiYXBpX2RvbWFpbiI6ImFwaS1iLmFtb2NybS5ydSJ9.lUNLpFwFRlGFE8wj0pE0sx5o-KmPRSph0oR0LJg0-KodUWNOkrQFJCUM25lnvlWXcjwD1iRtXdprdAS-nRKEeUDUMCcZ31L17YaXlT449p1A9nxoNL2ECjEa3OnIdiozSx2Mgzx_87Y_XU09iUEu5B0EFx25FLeCYiANVoHzYpHqThyVaKqKrAprRrm7mdQFnHbSqLgwJK3T2BJd-as-DJ0BNot3pJjaGlrqool8RfTbirmcXVKIEYetK-xrNYF6ixtFgC97IK9FXXJbegLZ4HMbYBYrH8WyHMCEy7cug7-KU5xwIk0cIZUb4-1J-X8MmOPRSfWypMK68kMQ6ySQjA");
-//
-// const requestOptions = {
-//   method: "GET",
-//   headers: myHeaders,
-//   redirect: "follow"
-// };
-//
-// fetch("./api/v4/leads", requestOptions)
-//   .then((response) => response.text())
-//   .then((result) => console.log(result))
-//   .catch((error) => console.error(error));
+import AmoCrmApiService from './api.js';
+import Offers from './offers.js';
+import {showAlert} from './utils.js';
+import {BEARER_TOKEN, END_POINT, MAX_LOADING_OFFERS, TIMOUT_LOAD} from './const.js';
+
+const offersApiService = new AmoCrmApiService({
+  endPoint: END_POINT,
+  bearerToken: BEARER_TOKEN,
+  loadLimit: MAX_LOADING_OFFERS,
+})
+
+const offers = new Offers({
+  offersApiService,
+  timeoutLoad: TIMOUT_LOAD,
+});
+
+offers.init()
+  .catch((error) => {
+    showAlert('Ошибка при загрузке. Обновите страницу');
+
+    throw error;
+  });
