ce14d5922be017752868cd9b5721150ae8199947
qostua <qostua@gmail.com>
2024-09-12 05:14:46 +0300

    made api class

---
+++ src/js/api.js
@@ -0,0 +1,74 @@
+export default class AmoCrmApiService {
+  #endPoint;
+  #bearerToken;
+  #loadLimit;
+
+  constructor({endPoint, bearerToken, loadLimit}) {
+    this.#endPoint = endPoint;
+    this.#bearerToken = bearerToken;
+    this.#loadLimit = loadLimit;
+  }
+
+  getOffers({
+    page = 1,
+  }) {
+    return this.#load({
+      url: 'leads',
+      params: {
+        limit: this.#loadLimit,
+        page,
+      }
+    })
+      .then(this.#parseResponse);
+  }
+
+  getOneOffer({
+    id
+  }) {
+    return this.#load({
+      url: `leads/${id}`,
+    })
+      .then(this.#parseResponse);
+  }
+
+  async #load({
+    url,
+    method = 'GET',
+    body = null,
+    headers = new Headers(),
+    params = {},
+  }) {
+    headers.append('Authorization', `Bearer ${this.#bearerToken}`);
+
+    const urlObject = new URL(`${this.#endPoint}/${url}`);
+    const urlParams = new URLSearchParams(params);
+
+    urlObject.search = urlParams.toString();
+
+    const response = await fetch(
+      urlObject,
+      {method, body, headers},
+    );
+
+    try {
+      this.#checkStatus(response);
+      return response;
+    } catch (err) {
+      this.#catchError(err);
+    }
+  }
+
+  #parseResponse(response) {
+    return response.json();
+  }
+
+  #checkStatus(response) {
+    if (!response.ok) {
+      throw new Error(`${response.status}: ${response.statusText}`);
+    }
+  }
+
+  #catchError(err) {
+    throw err;
+  }
+}
